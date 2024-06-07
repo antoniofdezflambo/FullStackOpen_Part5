f20c55a0b85d9ede695a676148d81690c878a7e8
antoniofdezflambo <antoniofdezflambo@gmail.com>
2024-06-07 14:39:05 +0200

    Exercise 5.14 completed

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,24 +1,38 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 import Blog from './Blog'
 
 describe('<Blog />', () => {
+  let blog, container
 
-  test('blow shows title and author, but not shows url and likes by default', () => {
-    const blog = {
+  beforeEach(() => {
+    blog = {
       title: 'Title for testing',
       author: 'Anonymous',
       url: 'https://testing.com',
-      likes: 0
+      likes: 0,
+      user: ''
     }
 
-    const { container } = render(<Blog blog={blog} />)
+    container = render(<Blog blog={blog} updateBlogs={() => {}} notifications={{}} />).container
+  })
 
-    screen.debug()
-
+  test('blow shows title and author, but not shows url and likes by default', () => {
     const element = container.querySelector('.blog')
     expect(element).toHaveTextContent('Title for testing - Anonymous')
     expect(element).not.toHaveTextContent('https://testing.com')
     expect(element).not.toHaveTextContent('Likes: 0')
   })
 
+  test('when details button is clicked, blog shows url and likes', async () => {
+    const user = userEvent.setup()
+
+    const button = screen.getByText('View')
+    await user.click(button)
+
+    const element = container.querySelector('.blog')
+    expect(element).toHaveTextContent('https://testing.com')
+    expect(element).toHaveTextContent('Likes: 0')
+  })
 })
